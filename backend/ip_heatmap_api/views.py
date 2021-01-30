7b25bcd9dc1e6309b48505e5633cb33b6a9d6dd0
haakensonb <untamedhawk44@gmail.com>
2021-01-30 01:43:00 -0500

    Able to make GET request with box parameters and get list of ip addresses

--- backend/ip_heatmap_api/views.py
+++ backend/ip_heatmap_api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from ip_heatmap_api.models import IPAddress
 from ip_heatmap_api.serializers import IPAddressSerializer
@@ -6,6 +6,16 @@ from ip_heatmap_api.serializers import IPAddressSerializer
 
 class IPAddressViewSet(viewsets.ViewSet):
     def list(self, request):
-        queryset = IPAddress.objects.all().first()
-        serializer = IPAddressSerializer(queryset)
-        return Response(serializer.data)
+        xmin = request.query_params.get('xmin')
+        ymin = request.query_params.get('ymin')
+        xmax = request.query_params.get('xmax')
+        ymax = request.query_params.get('ymax')
+        # Rudimentary boundry box check.
+        # TODO: Handle edge cases
+        if (xmin and ymin and xmax and ymax):
+            queryset = IPAddress.objects.filter(latitude__gte=xmin).filter(
+                latitude__lte=xmax).filter(longitude__gte=ymin).filter(longitude__lte=ymax)
+            serializer = IPAddressSerializer(queryset, many=True)
+            return Response(serializer.data)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
