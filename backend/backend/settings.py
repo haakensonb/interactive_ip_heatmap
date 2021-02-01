c08bf66e47602c362009aeda8aee9f1c20bb53b1
haakensonb <untamedhawk44@gmail.com>
2021-01-31 21:30:07 -0500

    Add cors headers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,10 +28,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'rest_framework',
     'ip_heatmap_api.apps.IpHeatmapApiConfig',
     'django.contrib.admin',
@@ -43,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
