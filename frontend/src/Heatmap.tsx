3e885a6687b8c2f41da57569c1a6d918593a529a
haakensonb <untamedhawk44@gmail.com>
2021-02-02 00:22:22 -0500

    Fix unneeded rendering for slightly better performance
    Api calls very slow

--- frontend/src/Heatmap.tsx
+++ frontend/src/Heatmap.tsx
@@ -1,20 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useMap } from "react-leaflet";
 import L, { HeatLatLngTuple, HeatMapOptions, LatLng, LatLngExpression, LatLngLiteral } from 'leaflet';
 import 'leaflet.heat';
 
 // import { IPAddressData, State } from './useApi';
 import { State } from './useApi';
+import { HeatmapProps } from "./HeatmapContainer";
 
-function Heatmap(props: State) {
+function Heatmap(props: HeatmapProps) {
     const map = useMap();
 
+    const layerRef = useRef(L.featureGroup());
+
+    // L.heatLayer(props.points ? props.points : [], {}).addTo(map);
+
     useEffect(() => {
-        console.log("points", props.data);
-        const points = (props.data !== undefined) ? props.data : [];
+        layerRef.current.clearLayers();
+
+        console.log("points", props.points);
+        const points = (props.points !== undefined) ? props.points : [];
 
         const options: HeatMapOptions = {
-            radius: 15,
+            radius: 10,
             gradient: {
                 0.0: 'green',
                 0.5: 'yellow',
@@ -23,8 +30,10 @@ function Heatmap(props: State) {
             minOpacity: 0.5
         };
 
-        L.heatLayer(points, options).addTo(map);
-    }, [map, props.data]); // Specifically check for changes in data?
+        // L.heatLayer(points, options).addTo(map);
+        L.heatLayer(points, options).addTo(layerRef.current);
+        layerRef.current.addTo(map);
+    }, [map, props.points]); // Specifically check for changes in data?
 
     return null;
 }
--- frontend/src/HeatmapContainer.tsx
+++ frontend/src/HeatmapContainer.tsx
@@ -1,17 +1,24 @@
 import { LatLng, LatLngBounds, LatLngExpression, LatLngLiteral } from "leaflet";
 import { useEffect, useState } from "react";
-import { useMapEvent } from "react-leaflet";
+import { useMap, useMapEvent } from "react-leaflet";
 import Heatmap from "./Heatmap";
 import useApi from "./useApi";
+import L from "leaflet";
+import "leaflet.heat";
 
 function urlBoundsFormat(bounds: LatLngBounds): string {
     return `http://127.0.0.1:8000/api/ipaddress/?xmin=${bounds.getSouthWest().lat}&ymin=${bounds.getSouthWest().lng}&xmax=${bounds.getNorthEast().lat}&ymax=${bounds.getNorthEast().lng}`;
 }
 
+export interface HeatmapProps {
+    points?: LatLng[];
+    bounds: LatLngBounds;
+}
+
 function HeatmapContainer() {
     const dummyBounds: LatLngBounds = new LatLngBounds([51.56, -0.11], [51.42, 0.6]);
     const [bounds, setBounds] = useState(dummyBounds);
-    const mult = 0.5;
+    const mult = 1;
     const getMaxBounds = (bounds: LatLngBounds, mult: number): LatLngBounds => {
         const sw: LatLngExpression = bounds.getSouthWest();
         const ne: LatLngExpression = bounds.getNorthEast();
@@ -30,24 +37,26 @@ function HeatmapContainer() {
         setBounds(newBounds);
     });
 
+    useEffect(() => {
+        setUrl(urlBoundsFormat(bounds));
+    }, [bounds, setUrl]);
 
+    // Def not working properly....
     // HACK: This is super bugged. None of the bounds checking in this app check for where it flips on the globe.
     // Not sure if this is working properly...
-    useEffect(() => {
-        console.log(bounds);
-        console.log(maxBounds);
-        if ((!maxBounds.contains(bounds.getSouthWest())) || (!maxBounds.contains(bounds.getNorthEast()))) {
-            console.log("Exceeded bounds");
-            setMaxBounds(getMaxBounds(bounds, mult));
-            const url = urlBoundsFormat(maxBounds);
-            setUrl(url);
-            console.log("result");
-            console.log(result);
-        }
-    }, [bounds, maxBounds, result, setUrl]);
+    // useEffect(() => {
+    //     console.log(bounds);
+    //     console.log(maxBounds);
+    //     if ((!maxBounds.contains(bounds.getSouthWest())) || (!maxBounds.contains(bounds.getNorthEast()))) {
+    //         console.log("Exceeded bounds");
+    //         setMaxBounds(getMaxBounds(bounds, mult));
+    //         const url = urlBoundsFormat(maxBounds);
+    //         setUrl(url);
+    //     }
+    // }, [bounds, maxBounds, setUrl, setMaxBounds]);
 
     return (
-        <Heatmap {...result} />
+        <Heatmap {...{ points: result.data, bounds: bounds }} />
     );
 }
 
